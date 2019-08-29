38a4615ff0fa5a08ae4a11c609aeff8f2ed385ab
Roman <romandulman@gmail.com>
2019-08-30 00:51:39 +0300

    Added code

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-    host: '192.168.2.14', //'172.17.0.2',
+    host:  '172.17.0.2',//'192.168.2.14' ,
     user: 'devadmin',
-    password: '41734173',
+    password: 'Logic41734173!',
     database: 'games'
 });
 module.exports = conn;
